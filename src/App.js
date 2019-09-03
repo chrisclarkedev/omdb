edb23017848aae8313ade755cdc7fbfe60ad2f2f
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-09-03 17:07:23 -0400

    changes to structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Link, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Header />
-      <Search />
-    </div>
+    <>
+      <div className="App">
+        <Navbar />
+        <Header />
+        <Search />
+      </div>
+
+    </>
   );
 }
 
