295744fbd3ac6aae1845fdcebcb16359bd808a6c
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-09-03 11:54:41 -0400

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <p>WELCOME TO THE OMDB</p>
     </div>
   );
