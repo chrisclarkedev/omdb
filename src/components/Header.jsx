01942bb2b065ab03b997299d52bb22654cbd2968
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-09-03 15:21:55 -0400

    styling on home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header>
       <section class="sec1"> <img src={require('./images/pic1.jpeg')} /></section>
-      <h5 className="banner-title2">Search for your favorite movie</h5>
-      {/* <div className="banner">
-        <img src={require('./images/pic1.jpeg')} />
-      </div> */}
+      <h2 className="banner-title1">Search for your favorite movie.</h2>
+      <h3 className="banner-title2">OMDb API</h3>
     </header>
   )
 };
