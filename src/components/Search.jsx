84387524efa6136e6e10c0ce3cc2453fa72a3a36
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-09-04 02:44:26 -0400

    added image route to api call

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -25,10 +25,12 @@ class Search extends React.Component {
         console.log(res);
         const title = res.data.Title;
         const year = res.data.Year;
+        const poster = res.data.Poster;
 
 
         this.setState({ title: title })
         this.setState({ year: year })
+        this.setState({ poster: poster })
       }).catch(err => {
 
       })
@@ -52,6 +54,7 @@ class Search extends React.Component {
         <Form getMovie={this.getMovie} />
         <h2>The title of the movie is{this.state.title}</h2>
         <h2>The year this came out was {this.state.year}</h2>
+        <img src={this.state.poster} />
       </>
     );
   }
