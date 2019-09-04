1baed8540480ba532b6c94dc5aae895e5677575f
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-09-04 00:08:09 -0400

    rearranged components but no styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Search from './components/Search';
+import { Link, Route } from 'react-router-dom';
+import Home from './components/Home';
+// import Header from './components/Header';
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <div className="App">
-        <Navbar />
-        <Header />
-        <Search />
+        <div className="nav">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><a href="/#search">Search</a></li>
+          </ul>
+          <main>
+            <Route path="/" exact render={() => <Home />} />
+          </main>
+        </div>
       </div>
 
     </>
